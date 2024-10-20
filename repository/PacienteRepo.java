30cf4293f918f90d1a54e20d756c4443a4269a22
santiago <santiagojesuslaureano369@gmail.com>
2024-10-20 12:55:52 -0600

    Implementacion de optional para buscar por CURP en la Bd

--- repository/PacienteRepo.java
+++ repository/PacienteRepo.java
@@ -1,9 +1,13 @@
 package com.utsem.consultorioSJLF.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.utsem.consultorioSJLF.Model.Paciente;
 
 public interface PacienteRepo extends JpaRepository<Paciente, Long> {
 
+	Optional<Paciente> findByCurpSJLF (String curp);
+	
 }
