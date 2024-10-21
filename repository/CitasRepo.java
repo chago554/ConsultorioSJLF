70e70b78fa40879be29161ac9fff0d7d9d890047
santiago <santiagojesuslaureano369@gmail.com>
2024-10-20 18:46:41 -0600

    Se impemento un atributo para contar el numero de citas que tiene un usuario, ya que causaba conflicto al retornar mas de 1 resultado

--- repository/CitasRepo.java
+++ repository/CitasRepo.java
@@ -1,10 +1,17 @@
 package com.utsem.consultorioSJLF.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.utsem.consultorioSJLF.Model.Cita;
+import com.utsem.consultorioSJLF.Model.Paciente;
+
+
+
 
 public interface CitasRepo extends JpaRepository<Cita, Long> {
 
+	Optional<Cita> findByPaciente(Paciente paciente);
+	Long countByPaciente(Paciente paciente);
+	
 }
 	
