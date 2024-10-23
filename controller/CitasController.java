fe9a144513c680839abbb71a58492b78701e5d0c
santiago <santiagojesuslaureano369@gmail.com>
2024-10-23 13:58:42 -0600

    Se crea el endpoint para actualizar el estatus de la cita

--- controller/CitasController.java
+++ controller/CitasController.java
@@ -1,9 +1,11 @@
 package com.utsem.consultorioSJLF.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,17 +13,30 @@ import org.springframework.web.bind.annotation.RestController;
 import com.utsem.consultorioSJLF.Model.Cita;
 import com.utsem.consultorioSJLF.repository.CitasRepo;
 
-
 @RestController
 @RequestMapping("citasSJLF")
 public class CitasController {
 
 	@Autowired
 	CitasRepo citasRepo;
-	
+
+	// listar
 	@PostMapping("listarSJLF")
 	public List<Cita> postListar() {
 		return citasRepo.findAll();
 	}
-	
+
+	//
+	@PostMapping("cambiarEstatusSJLF")
+	public String cambiarEstatusSJLF(@RequestBody Cita cita) {
+		Optional<Cita> laCita = citasRepo.findById(cita.getId());
+		if (laCita.isPresent()) {
+			laCita.get().setEstatus(cita.getEstatus());
+			citasRepo.save(laCita.get());
+			return "Estatus actualizado";
+		}
+
+		return "La cita no existe";
+	}
+
 }
