e9ab44819b3628cfb86d63f80679f8f4e75decc9
santiago <santiagojesuslaureano369@gmail.com>
2024-11-07 11:45:54 -0600

    Se actualiza y con un try cathc se valida la eliminacion de un medico con  otros tablas

--- controller/MedicoController.java
+++ controller/MedicoController.java
@@ -11,9 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 @RequestMapping("medicosSJLF")
@@ -21,94 +21,93 @@ public class MedicoController {
 
 	@Autowired
 	MedicoRepo medicoRepo;
-	
-	
-	//consultar
+
+	// consultar
 	@PostMapping("listarSJLF")
 	public List<Medico> listarMedicos() {
 		return medicoRepo.findAll();
 	}
-	
+
 	// guardar
 	@PostMapping("guardarSJLF")
 	public String postCrear(@RequestBody Medico medico) {
-		Optional<Medico> elMedico =  medicoRepo.findByCedulaSJLF(medico.getCedulaSJLF());
-		if(elMedico.isPresent()) {
+		Optional<Medico> elMedico = medicoRepo.findByCedulaSJLF(medico.getCedulaSJLF());
+		if (elMedico.isPresent()) {
 			return "¡La cédula profesional ya existe!";
 		}
-		
-		if (medico.getCedulaSJLF().isEmpty() || medico.getNombreSJLF().isEmpty() || medico.getApellidosSJLF().isEmpty() ) {
+
+		if (medico.getCedulaSJLF().isEmpty() || medico.getNombreSJLF().isEmpty()
+				|| medico.getApellidosSJLF().isEmpty()) {
 			return "¡Por favor, rellene los campos obligatorios!";
 		}
-		if(medico.getCedulaSJLF().length()< 8 ) {
+		if (medico.getCedulaSJLF().length() < 8) {
 			return "¡La cédula profesional no cumple con la longitud mínima!";
 		}
 		medicoRepo.save(medico);
 		return "¡Médico guardado con exito!";
 
 	}
-	
+
 	// consultar a un medico especifico
-		@PostMapping("consultarSJLF")
-		public Medico postMethodName(@RequestBody Medico medico) {
-			Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
-			if (elMedico.isPresent()) {
-				return elMedico.get();
-			}
-			return new Medico();
+	@PostMapping("consultarSJLF")
+	public Medico postMethodName(@RequestBody Medico medico) {
+		Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
+		if (elMedico.isPresent()) {
+			return elMedico.get();
 		}
-	
+		return new Medico();
+	}
+
 	// Eliminar
-		@Autowired
-		CitasRepo citasRepo;
+	@Autowired
+	CitasRepo citasRepo;
 
-		@PostMapping("eliminarSJLF")
-		public String postEliminar(@RequestBody Medico medico) {
-			Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
+	@PostMapping("eliminarSJLF")
+	public String postEliminar(@RequestBody Medico medico) {
+		Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
 
-			Long citas = citasRepo.countByMedico(medico);
-			if (citas >=1) {
-				return "¡No se puede eliminar a este médico!";
-			}
-			if (elMedico.isPresent()) {
+		if (elMedico.isPresent()) {
+			try {
 				medicoRepo.delete(elMedico.get());
 				return "¡Se eliminó el médico correctamente!";
+			} catch (DataIntegrityViolationException e) {
+				return "Este registro está conectado a otros datos, por lo que no se puede eliminar";
 			}
-			return "¡No existe este médico!";
 		}
-
-		// modificar
-		@PostMapping("modificarSJLF")
-		public String postModificar(@RequestBody Medico medico) {
-
-			Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
-			Optional<Medico> laCedula = medicoRepo.findByCedulaSJLF(medico.getCedulaSJLF());
-
-			if (elMedico.isPresent()) {
-
-				if (medico.getNombreSJLF().isEmpty() || medico.getCedulaSJLF().isEmpty()
-						|| medico.getApellidosSJLF().isEmpty()) {
-					return "¡Por favor, rellene los campos obligatorios!";
-				}
-
-				if (medico.getCedulaSJLF().length() < 8) {
-					return "¡La longitud de la cédula esta incompleta!";
-				}
-
-				if (laCedula.isPresent() && laCedula.get().getId() != medico.getId()) {
-					return "¡La cédula profesional ya existe!";
-				}
-
-				elMedico.get().setNombreSJLF(medico.getNombreSJLF());
-				elMedico.get().setCedulaSJLF(medico.getCedulaSJLF());
-				elMedico.get().setApellidosSJLF(medico.getApellidosSJLF());
-
-				medicoRepo.save(elMedico.get());
-				return "¡Médico actualizado exitosamente!";
-
-			}
-			return "El médico NO existe";
-		}
-
-		
+		return "¡No existe este médico!";
 	}
+
+	// modificar
+	@PostMapping("modificarSJLF")
+	public String postModificar(@RequestBody Medico medico) {
+
+		Optional<Medico> elMedico = medicoRepo.findById(medico.getId());
+		Optional<Medico> laCedula = medicoRepo.findByCedulaSJLF(medico.getCedulaSJLF());
+
+		if (elMedico.isPresent()) {
+
+			if (medico.getNombreSJLF().isEmpty() || medico.getCedulaSJLF().isEmpty()
+					|| medico.getApellidosSJLF().isEmpty()) {
+				return "¡Por favor, rellene los campos obligatorios!";
+			}
+
+			if (medico.getCedulaSJLF().length() < 8) {
+				return "¡La longitud de la cédula esta incompleta!";
+			}
+
+			if (laCedula.isPresent() && laCedula.get().getId() != medico.getId()) {
+				return "¡La cédula profesional ya existe!";
+			}
+
+			elMedico.get().setNombreSJLF(medico.getNombreSJLF());
+			elMedico.get().setCedulaSJLF(medico.getCedulaSJLF());
+			elMedico.get().setApellidosSJLF(medico.getApellidosSJLF());
+
+			medicoRepo.save(elMedico.get());
+			return "¡Médico actualizado exitosamente!";
+
+		}
+		return "El médico NO existe";
+	}
+
+}
