8ae11dd1408e1a2c989b2ceba565e01dc82f3c45
santiago <santiagojesuslaureano369@gmail.com>
2024-11-07 08:28:26 -0600

    Se busca por fecha asc

--- repository/CitasRepo.java
+++ repository/CitasRepo.java
@@ -7,17 +7,13 @@ import com.utsem.consultorioSJLF.Model.Cita;
 import com.utsem.consultorioSJLF.Model.Medico;
 import com.utsem.consultorioSJLF.Model.Paciente;
 
-
-
-
 public interface CitasRepo extends JpaRepository<Cita, Long> {
 
 	Optional<Cita> findByPaciente(Paciente paciente);
 	Long countByPaciente(Paciente paciente);
 	Optional<Cita> findByMedico(Medico medico);
 	Long countByMedico(Medico medico);
+
+	List<Cita> findByOrderByFechaSJLFAsc();
 	
-	
-	 List<Cita> findByOrderByFechaSJLFAsc();
 }
-	
