94dc68fad598c6c16732a55f369dc13098751013
santiago <santiagojesuslaureano369@gmail.com>
2024-11-14 08:12:19 -0600

    Creacion del repositorio de consulta

---
+++ repository/ConsultaRepository.java
@@ -0,0 +1,9 @@
+package com.utsem.consultorioSJLF.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.utsem.consultorioSJLF.Model.Consulta;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+}
