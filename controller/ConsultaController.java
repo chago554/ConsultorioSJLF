ae29b3a417c72b406674bdb469fc111ab2d349e3
santiago <santiagojesuslaureano369@gmail.com>
2024-11-17 05:20:26 -0600

    Corregir el endpoint de guardar

--- controller/ConsultaController.java
+++ controller/ConsultaController.java
@@ -2,21 +2,41 @@ package com.utsem.consultorioSJLF.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import com.utsem.consultorioSJLF.Model.Consulta;
 import com.utsem.consultorioSJLF.repository.ConsultaRepository;
+
 @RestController
 @RequestMapping("consultasSJLF")
 public class ConsultaController {
-	
+
 	@Autowired
 	ConsultaRepository consultaRepository;
+
+	// guardar
+	@PostMapping("guadarSJLF")
+	public String postMethodName(@RequestBody Consulta consulta) {
+		
+		try {
+			
+			if(consulta.getSintomasSJLF().isEmpty()) {
+				return "¡Rellene todos los campos!";
+			}
+			
+			consultaRepository.save(consulta);
+			return "¡Cita creada exitosamente!";
+			
+		} catch (Exception e) {
+			return "Error:" + e ;
+		}
+		
 	
+		
 	
 
-
-	
+	}
 
 }
